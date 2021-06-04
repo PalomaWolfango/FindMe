467cc7ba2a3a4aa8cc1a6385147abad6efd41145
PalomaWolfango <paloma.wolfango@gmail.com>
2021-06-04 14:49:34 +0100

    Primeiro commit no development

--- app/src/main/java/ipvc/estg/findme/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findme/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    //Classe Main
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
     }
 }
