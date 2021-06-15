98e3f71152f828caec44b804a55fe40215dedb05
Alexandra Castro <alexandracastro@ipvc.pt>
2021-06-16 00:16:41 +0100

    Notificacoes1

--- app/src/main/java/ipvc/estg/findme/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findme/MainActivity.kt
@@ -2,6 +2,12 @@ package ipvc.estg.findme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.ktx.messaging
 
 class MainActivity : AppCompatActivity() {
     //Classe Main
@@ -9,6 +15,33 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
+
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.e("TAG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // Log and toast
+            val msg = token
+            if (msg != null) {
+                Log.e("TAG", msg)
+            }
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
+
+        Firebase.messaging.subscribeToTopic("animais")
+            .addOnCompleteListener { task ->
+                var msg ="subscriot"
+                if (!task.isSuccessful) {
+                    msg = "nao subscriot"
+                }
+                Log.e("TAG", msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            }
 
         //teste commit
     }
