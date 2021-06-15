e00c6348d54d4ffd1d137069079ada6e98bceeeb
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-06-15 21:47:27 +0100

    teste

--- app/src/main/java/ipvc/estg/findme/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findme/MainActivity.kt
@@ -1,19 +1,33 @@
 package ipvc.estg.findme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.content.Intent
+import android.hardware.SensorManager
+import androidx.appcompat.app.AppCompatDelegate
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    //Classe Main
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.hide()
+      //  supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
-        //teste git
 
 
-        //login
+        login_button_login.setOnClickListener {
+            val email = insertUser.text.toString()
+            val password = insertPassword.text.toString()
+
+
+            Log.d("Login", "tentar login : $email/***")
+            Log.d("Login", "tentar login : $password/***")
+        }
 
     }
+
 }
