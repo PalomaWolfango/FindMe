6066d441a50395ef53f1e48457710f64a6b109b3
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-06-16 18:58:21 +0100

    começar o chat

--- app/src/main/java/ipvc/estg/findme/NewMessageActivity.kt
+++ app/src/main/java/ipvc/estg/findme/NewMessageActivity.kt
@@ -2,7 +2,10 @@ package ipvc.estg.findme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
@@ -13,7 +16,23 @@ class NewMessageActivity : AppCompatActivity() {
         supportActionBar?.title = "Selecionar Utilizador"
 
 
-        recyclerview_newmessage.adapter
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        recyclerview_newmessage.adapter = adapter
 
     }
+}
+
+class UserItem: Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+    }
+
+    override fun getLayout(): Int {
+       return R.layout.user_row_new_message
+    }
+
 }
