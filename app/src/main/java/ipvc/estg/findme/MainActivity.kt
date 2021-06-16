71b93ef2bf33d15c15c6b2969681952832773e27
monicalima18666 <61920974+monicalima18666@users.noreply.github.com>
2021-06-16 14:46:19 +0100

    login feito

--- app/src/main/java/ipvc/estg/findme/MainActivity.kt
+++ app/src/main/java/ipvc/estg/findme/MainActivity.kt
@@ -1,15 +1,25 @@
 package ipvc.estg.findme
 
+import android.content.ContentValues.TAG
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.content.Intent
 import android.hardware.SensorManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,17 +27,44 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
+        auth = Firebase.auth
+
 
 
         btn.setOnClickListener {
-            val email = insertUser.text.toString()
-            val password = insertPassword.text.toString()
+            performLogin()
 
-
-            Log.d("Login", "tentar login : $email/***")
-            Log.d("Login", "tentar login : $password/***")
         }
 
     }
 
-}
+    private fun performLogin() {
+        val email = insertUser.text.toString()
+        val password = insertPassword.text.toString()
+
+
+        Log.d("Login", "tentar login : $email/***")
+         Log.d("Login", "tentar login : $password/***")
+
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill out email/pw.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (!it.isSuccessful) return@addOnCompleteListener
+
+                Log.d("Login", "Successfully logged in: ${it.result?.user?.uid}")
+
+                val intent = Intent(this, LatestMessagesActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, "Failed to log in: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
