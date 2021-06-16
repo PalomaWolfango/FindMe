4c896aa95cd2d674e0059ff2c7c91a88af42ed4a
Pedro Ferreira <pedromiguel2.sferreira@gmail.com>
2021-06-16 19:56:55 +0100

    Commit inicial inserir desaparecimento

---
+++ app/src/main/java/ipvc/estg/findme/InserirDesaparecimento.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.findme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InserirDesaparecimento : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inserir_desaparecimento)
+    }
+}
